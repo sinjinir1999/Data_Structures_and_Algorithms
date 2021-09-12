ac09841981719d8601d5ad8bc092fa0608c977a5
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-12 18:05:59 +0530

    Sort Colors [75]

---
+++ Arrays/1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        int mid=0;
+        while(mid<=h)
+        {
+            switch(nums[mid])
+            {
+                case 0:
+                    swap(nums[l++],nums[mid++]);
+                    break;
+                case 1: mid++;
+                    break;
+                case 2: 
+                    swap(nums[mid],nums[h--]);
+                    break;
+            }
+        }
+        
+    }
+};
